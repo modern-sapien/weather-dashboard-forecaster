d2256acd9f9dde8ffbc4f969a40286857870a5d6
Jonathan Canales <joncanales2@gmail.com>
2020-09-12 15:14:57 -0400

    did targeting for finding what was necessary in the object then had to look further into documentation and found that I had to use a different API request url.

--- weather.js
+++ weather.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-
-
+var currentDateText = $("#current-Day")
 var forecastDisplay = $("#current-forecast");
 var futureForecast = $("#future-forecast");
 
@@ -11,21 +10,36 @@ $("#search-button").on("click", function()  {
     console.log("thanks amiel"); 
  
     $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchVal + "&appid=fe5d52c1ddca1663f39aaaddd939123d",
+        url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchVal + "&units=imperial&appid=fe5d52c1ddca1663f39aaaddd939123d",
         method: "GET", 
         dataType: "json"
     }).then(function(response){
-        console.log(response)
-        console.log(response.name)
-        console.log(response.wind.speed)
+        console.log(response.coord.lat); // latitude
+        console.log(response.coord.lon);   // longitidunal
+        
+        var searchLat = response.coord.lat;
+        var searchLon = response.coord.lon;
+
+        console.log(searchLat);
+        console.log(searchLon);
+    });
 
 
 
-    })
+
+
 });
 
 
 });
+
+
+        // GOING TO SEE IF THIS WORKS WITH THE LATITUDE SEARCH
+        // console.log(response.name) // city name
+        // console.log(response.main.temp) // speed in MPH
+        // console.log(response.wind.speed) // speed in MPH
+        // console.log(response.weather[0].icon) // weather icon
+        // console.log(response.main.humidity + " i'm humidity") // humidity index
 
 
 
